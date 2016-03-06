2c178ae70262c335e0c8fb5816d258741dfea48b
Xavier Harris <xharris1@umbc.edu>
2016-03-05 23:24:08 -0500

    moved index.css file to css folder

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
     <!-- CSS and SCRIPT includes -->
     <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
     <link rel="stylesheet" type="text/css" href="css/container_user_list.css">
     <script src="https://maps.googleapis.com/maps/api/js"></script>
 
