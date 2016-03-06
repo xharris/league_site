6fdbf18940d6e71bd9e36c7c96cf09a31744710b
Xavier Harris <xharris1@umbc.edu>
2016-03-06 02:35:06 -0500

    stuff

--- index.php
+++ index.php
@@ -8,7 +8,12 @@
     <link rel="stylesheet" type="text/css" href="css/container_user_list.css">
     <script src="https://maps.googleapis.com/maps/api/js"></script>
 
-    <?php require_once "load.php"; ?>
+    <?php
+        require_once "load.php";
+
+        $cities = $DB->getCities();
+
+    ?>
 
     <table border="1" style="width:100%" id="main">
 
@@ -57,13 +62,13 @@
 <td class="country">
 
                     <?php
-                        $states = ['ALABAMA','ALASKA','ARIZONA','ARKANSAS','CALIFORNIA','COLORADO','CONNECTICUT','DELAWARE','DISTRICT OF COLUMBIA','FEDERATED STATES OF MICRONESIA','FLORIDA','GEORGIA','HAWAII','IDAHO','ILLINOIS','INDIANA','IOWA','KANSAS','KENTUCKY','LOUISIANA','MAINE','MARYLAND','MASSACHUSETTS','MICHIGAN','MINNESOTA','MISSISSIPPI','MISSOURI','MONTANA','NEBRASKA','NEVADA','NEW HAMPSHIRE','NEW JERSEY','NEW MEXICO','NEW YORK','NORTH CAROLINA','NORTH DAKOTA','OHIO','OKLAHOMA','OREGON','PENNSYLVANIA','PUERTO RICO','RHODE ISLAND','SOUTH CAROLINA','SOUTH DAKOTA','TENNESSEE','TEXAS','UTAH','VERMONT','VIRGIN ISLANDS','VIRGINIA','WASHINGTON','WEST VIRGINIA','WISCONSIN','WYOMING',];
+                        $states = array('ALABAMA','ALASKA','ARIZONA','ARKANSAS','CALIFORNIA','COLORADO','CONNECTICUT','DELAWARE','DISTRICT OF COLUMBIA','FEDERATED STATES OF MICRONESIA','FLORIDA','GEORGIA','HAWAII','IDAHO','ILLINOIS','INDIANA','IOWA','KANSAS','KENTUCKY','LOUISIANA','MAINE','MARYLAND','MASSACHUSETTS','MICHIGAN','MINNESOTA','MISSISSIPPI','MISSOURI','MONTANA','NEBRASKA','NEVADA','NEW HAMPSHIRE','NEW JERSEY','NEW MEXICO','NEW YORK','NORTH CAROLINA','NORTH DAKOTA','OHIO','OKLAHOMA','OREGON','PENNSYLVANIA','PUERTO RICO','RHODE ISLAND','SOUTH CAROLINA','SOUTH DAKOTA','TENNESSEE','TEXAS','UTAH','VERMONT','VIRGIN ISLANDS','VIRGINIA','WASHINGTON','WEST VIRGINIA','WISCONSIN','WYOMING');
                         $cc = count($states);
                         for($i = 0 ; $i < $cc ; $i++){
                             /*echo '<tr><td class="state" colspan="3"> array_values($states['.$i.'])</td></tr>';*/
                             echo '<tr><td class="state" colspan="3">'.$states[$i].'</td></tr>';
                         }
-                        
+
                         ?>
 
                     <tr><td class="county" colspan="3">County</td></tr>
@@ -81,15 +86,83 @@
         </tr>
     </table>
 
+<?php
+    $cities_str = '[';
+
+    foreach ($cities as $city) {
+        $cities_str .= '"'.$city.'",';
+    }
+    $cities_str .= ']';
+ ?>
+
 <script>
     var mapCanvas, mapOptions, map, geocoder;
     var user_name, addr_suggest, long, lat;
+    var cities;
 
     $(function(){
         mapCanvas = document.getElementById("map");
-        mapOptions = {center: new google.maps.LatLng(38.8, -79.5), zoom: 4};
-        map = new google.maps.Map(mapCanvas, mapOptions);
+        map = new google.maps.Map(mapCanvas);
         geocoder = new google.maps.Geocoder;
+
+        cities = <?php echo $cities_str; ?>;
+
+
+      // add marker for cities with players
+      for (city in cities) {
+        geocoder.geocode( { 'address': cities[city]}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+              if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
+                var infowindow = new google.maps.InfoWindow(
+                    { content: '<b>'+city+'</b>',
+                      size: new google.maps.Size(150,50)
+                    });
+
+                var marker = new google.maps.Marker({
+                    position: results[0].geometry.location,
+                    map: map,
+                    title:cities[city]
+                });
+                google.maps.event.addListener(marker, 'click', function() {
+                    infowindow.open(map,marker);
+                });
+
+              } else {
+                alert("No results found");
+              }
+            } else {
+              alert("Geocode was not successful for the following reason: " + status);
+            }
+          });
+      }
+
+      // Get the user's location
+    if(navigator.geolocation) {
+      browserSupportFlag = true;
+      navigator.geolocation.getCurrentPosition(function(position) {
+        initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+        map.setCenter(initialLocation);
+        map.setZoom(10);
+
+        geocoder.geocode({'location': initialLocation}, function(results, status) {
+            // suggest it to them (lol)
+            addr_suggest = results[2].formatted_address;
+            $("#location").val(addr_suggest);
+
+            long = results[0].geometry.location.lng();
+            lat = results[0].geometry.location.lat();
+        });
+
+      }, function() {
+        handleNoGeolocation(browserSupportFlag);
+      });
+    }
+    // Browser doesn't support Geolocation
+    else {
+      browserSupportFlag = false;
+      handleNoGeolocation(browserSupportFlag);
+    }
+
     });
 
     // prevent page reload
@@ -150,33 +223,8 @@
         $(".country").hide();
         $(".county").show();
     });
-                    
-    // Get the user's location
-  if(navigator.geolocation) {
-    browserSupportFlag = true;
-    navigator.geolocation.getCurrentPosition(function(position) {
-      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-      map.setCenter(initialLocation);
-      map.setZoom(15);
 
-      geocoder.geocode({'location': initialLocation}, function(results, status) {
-          // suggest it to them (lol)
-          addr_suggest = results[2].formatted_address;
-          $("#location").val(addr_suggest);
 
-          long = results[0].geometry.location.lng();
-          lat = results[0].geometry.location.lat();
-      });
-
-    }, function() {
-      handleNoGeolocation(browserSupportFlag);
-    });
-  }
-  // Browser doesn't support Geolocation
-  else {
-    browserSupportFlag = false;
-    handleNoGeolocation(browserSupportFlag);
-  }
 
   function handleNoGeolocation(errorFlag) {
     if (errorFlag == true) {
--- load.php
+++ load.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 include("db_functions.php");
 
 $DB = new Database();
